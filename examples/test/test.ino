8a586110ff591e8b8cb71b74e9438dc2649fe3c0
Paul <Pbartek@users.noreply.github.com>
2015-09-08 08:30:34 -0500

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -3,7 +3,7 @@
  
  test.ino
  
- Program for writing to Newhaven Display 160x128 Graphic Color OLED with SEPS525 controller.
+ Program for writing to Newhaven Display's 160x128 Graphic Color OLED with SEPS525 controller.
  
  Pick one up today in the Newhaven Display shop!
  ------> http://www.newhavendisplay.com/nhd169160128ugc3-p-5603.html
